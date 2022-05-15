278ec8b5f63a2cf15ba342eeab3b56f5b668b962
Saúl Sáyago <sayagoadd@gmail.com>
2022-05-15 07:34:00 -0500

    eliminando la carpeta incorrecta

--- lectures/exercise/func-literal/func-literal.go
+++ lectures/exercise/func-literal/func-literal.go
@@ -19,18 +19,6 @@
 package main
 
 import "fmt"
-import "unicode"
-
-type LineCallback func(line string)
-
-//* Create a single function to iterate over each line of text that is
-//  provided in main().
-func lineIterator(lines []string, callback LineCallback) {
-	for i := 0; i < len(lines); i++ {
-		//  - The function must return nothing and must execute a closure
-		callback(lines[i])
-	}
-}
 
 func main() {
 	lines := []string{
@@ -40,40 +28,4 @@ func main() {
 		"12 spaces,",
 		"and 4 punctuation marks in these lines of text!",
 	}
-
-	letters := 0
-	numbers := 0
-	punctuation := 0
-	spaces := 0
-
-	//* Using closures, determine the following information about the text and
-	//  print a report to the terminal:
-	//  - Number of letters
-	//  - Number of digits
-	//  - Number of spaces
-	//  - Number of punctuation marks
-	lineFunc := func(line string) {
-		for _, r := range line {
-			if unicode.IsLetter(r) {
-				letters += 1
-			}
-			if unicode.IsDigit(r) {
-				numbers += 1
-			}
-			if unicode.IsPunct(r) {
-				punctuation += 1
-			}
-			if unicode.IsSpace(r) {
-				spaces += 1
-			}
-		}
-	}
-
-	lineIterator(lines, lineFunc)
-
-	// report
-	fmt.Println(letters, "letters")
-	fmt.Println(numbers, "digits")
-	fmt.Println(spaces, "spaces")
-	fmt.Println(punctuation, "punctuation marks")
 }
